3aac2dc56e5ef3dab3db699246052aa706bb6b0d
SayedZeeshanHyder <zhyder133@gmail.com>
2024-06-24 21:11:35 +0530

    Added Shimmer Effect

--- Flutter/gloriuspurpose/lib/screens/home/homescreen.dart
+++ Flutter/gloriuspurpose/lib/screens/home/homescreen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:gloriuspurpose/colors.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:gloriuspurpose/models/campaignmodel.dart';
 import 'package:gloriuspurpose/widgets/campaigncard.dart';
 
@@ -14,11 +15,37 @@ class HomeScreen extends StatelessWidget {
       stream: firestore.collection("Campaigns").doc("allCampaigns").snapshots(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(
-              color: myGreen,
-            ),
-          );
+          return Shimmer(loop: 0,
+          direction: ShimmerDirection.btt,
+          child: ListView.builder(itemCount: 10,itemBuilder: (context,index){
+            return Container(
+              width: size.width,
+              height: size.height*0.3,
+              margin: EdgeInsets.symmetric(horizontal: size.width * 0.04,),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: size.width * 0.03, vertical: size.height * 0.02),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        width: size.width * 0.9,
+                        height: size.height * 0.24,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),gradient: LinearGradient(colors: [Colors.grey.shade200,Colors.grey.shade300,Colors.grey.shade400],begin: Alignment.topLeft,end: Alignment.bottomRight),);
         }
 
         List data = [];
